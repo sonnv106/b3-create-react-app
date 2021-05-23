c13a9fe46f62316490f17157d920fb59c1268b19
sonnguyen <sonnguyen106mh@gmail.com>
2021-05-23 10:06:41 +0700

    add todos list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = ["Go to market", "Buy food", "Make dinner"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello world
-        </p>
+        <ul>
+          {todos.map(todo=>
+            <li>{todo}</li>)}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
